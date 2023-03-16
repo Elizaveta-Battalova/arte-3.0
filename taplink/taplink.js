ae9c07c1c4f34311af9f903a2e0804d97df37612
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2023-03-16 18:08:11 +0100

    new conserts

--- taplink/taplink.js
+++ taplink/taplink.js
@@ -39,21 +39,21 @@ const scrollPosition = () =>
   window.pageYOffset || document.documentElement.scrollTop;
 const containHide = () => header.classList.contains("hide");
 
-window.addEventListener("scroll", () => {
-  if (
-    scrollPosition() > lastScroll &&
-    !containHide() &&
-    scrollPosition() > defaultOffset
-  ) {
-    //scroll down
-    header.classList.add("hide");
-  } else if (scrollPosition() < lastScroll && containHide()) {
-    //scroll up
-    header.classList.remove("hide");
-  }
+// window.addEventListener("scroll", () => {
+//   if (
+//     scrollPosition() > lastScroll &&
+//     !containHide() &&
+//     scrollPosition() > defaultOffset
+//   ) {
+//     //scroll down
+//     header.classList.add("hide");
+//   } else if (scrollPosition() < lastScroll && containHide()) {
+//     //scroll up
+//     header.classList.remove("hide");
+//   }
 
-  lastScroll = scrollPosition();
-});
+//   lastScroll = scrollPosition();
+// });
 
 $(function () {
     toggleMenu();
@@ -66,134 +66,39 @@ $(function () {
     }
   });
 
-  function setSlider(){
-	
-	$(".slider").each( function(){
-		
-		var $slider = $(this),
-				$itemscontainer = $slider.find(".slider-items-container");
-		
-		if ($itemscontainer.find(".slider-item.active").length == 0){
-			$itemscontainer.find(".slider-item").first().addClass("active");
-		}
-		
-		function setWidth(){
-			var totalWidth = 0
-			
-			$($itemscontainer).find(".slider-item").each( function(){
-				totalWidth += $(this).outerWidth();
-			});
-			
-			$itemscontainer.width(totalWidth);
-			
-		}
-		function setTransform(){
-			
-			var $activeItem = $itemscontainer.find(".slider-item.active"),
-					activeItemOffset = $activeItem.offset().left,
-					itemsContainerOffset = $itemscontainer.offset().left,
-					totalOffset = activeItemOffset - itemsContainerOffset
-			
-			$itemscontainer.css({"transform": "translate( -"+totalOffset+"px, 0px)"})
-			
-		}
-		function nextSlide(){
-			var activeItem = $itemscontainer.find(".slider-item.active"),
-					activeItemIndex = activeItem.index(),
-					sliderItemTotal = $itemscontainer.find(".slider-item").length,
-					nextSlide = 0;
-			
-			if (activeItemIndex + 1 > sliderItemTotal - 1){
-				nextSlide = 0;
-			}else{
-				nextSlide = activeItemIndex + 1
-			}
-			
-			var nextSlideSelect = $itemscontainer.find(".slider-item").eq(nextSlide),
-					itemContainerOffset = $itemscontainer.offset().left,
-					totalOffset = nextSlideSelect.offset().left - itemContainerOffset
-			
-			$itemscontainer.find(".slider-item.active").removeClass("active");
-			nextSlideSelect.addClass("active");
-			$slider.find(".dots").find(".dot").removeClass("active")
-			$slider.find(".dots").find(".dot").eq(nextSlide).addClass("active");
-			$itemscontainer.css({"transform": "translate( -"+totalOffset+"px, 0px)"})
-			
-		}
-		function prevSlide(){
-			var activeItem = $itemscontainer.find(".slider-item.active"),
-					activeItemIndex = activeItem.index(),
-					sliderItemTotal = $itemscontainer.find(".slider-item").length,
-					nextSlide = 0;
-			
-			if (activeItemIndex - 1 < 0){
-				nextSlide = sliderItemTotal - 1;
-			}else{
-				nextSlide = activeItemIndex - 1;
-			}
-			
-			var nextSlideSelect = $itemscontainer.find(".slider-item").eq(nextSlide),
-					itemContainerOffset = $itemscontainer.offset().left,
-					totalOffset = nextSlideSelect.offset().left - itemContainerOffset
-			
-			$itemscontainer.find(".slider-item.active").removeClass("active");
-			nextSlideSelect.addClass("active");
-			$slider.find(".dots").find(".dot").removeClass("active")
-			$slider.find(".dots").find(".dot").eq(nextSlide).addClass("active");
-			$itemscontainer.css({"transform": "translate( -"+totalOffset+"px, 0px)"})
-			
-		}
-		function makeDots(){
-			var activeItem = $itemscontainer.find(".slider-item.active"),
-					activeItemIndex = activeItem.index(),
-					sliderItemTotal = $itemscontainer.find(".slider-item").length;
-			
-			for (i = 0; i < sliderItemTotal; i++){
-				$slider.find(".dots").append("<div class='dot'></div>")
-			}
-			
-			$slider.find(".dots").find(".dot").eq(activeItemIndex).addClass("active")
-			
-		}
-		
-		setWidth();
-		setTransform();
-		makeDots();
-		
-		$(window).resize( function(){
-					setWidth();
-					setTransform();
-		});
-		
-		var nextBtn = $slider.find(".controls").find(".next-btn"),
-				prevBtn = $slider.find(".controls").find(".prev-btn");
-		
-		nextBtn.on('click', function(e){
-			e.preventDefault();
-			nextSlide();
-		});
-		
-		prevBtn.on('click', function(e){
-			e.preventDefault();
-			prevSlide();
-		});
-		
-		$slider.find(".dots").find(".dot").on('click', function(e){
-			
-			var dotIndex = $(this).index(),
-					totalOffset = $itemscontainer.find(".slider-item").eq(dotIndex).offset().left - $itemscontainer.offset().left;
-					
-			$itemscontainer.find(".slider-item.active").removeClass("active");
-			$itemscontainer.find(".slider-item").eq(dotIndex).addClass("active");
-			$slider.find(".dots").find(".dot").removeClass("active");
-			$(this).addClass("active")
-			
-			$itemscontainer.css({"transform": "translate( -"+totalOffset+"px, 0px)"})
-			
-		});
-		
-	});
-	
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
 }
 
-setSlider();
+
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("item");
+    
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+  
+  /* Проходим по каждому слайду в цикле for */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }   
+    slides[slideIndex - 1].style.display = "block"; 
+}
